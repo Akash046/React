70d777cefed87d216b2ec64d5bf8aa1836821c6b
Akash Git <royalakash098@gmail.com>
2024-10-27 12:29:01 +0530

    Interview

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+
+function App() {
+  const username = "Thunder"
+  return (
+    <>
+    <Chai/>
+    <h1>This is {username}</h1>
+    <p>Test Paragraph</p>
+    </>
+    
+    
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function Card(username) {
+    console.log(username)
+    //console.log("props", props);
+    return (
+        <div>Card</div>
+    )
+}
+
+export default Card
